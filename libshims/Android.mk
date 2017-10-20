6fb2682252785bb837c352bf1ef2850a9c41b440
jrior001 <jriordan001@gmail.com>
2017-10-20 06:00:00 +0300

    b1w-common: convert CameraParameters header to shim lib
    Change-Id: If02fcb506da12ab4b8a0996d97da970db8c9c43c

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -12,25 +12,38 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-    camera_shim.cpp \
-    atomic.cpp
+    camera_shim.cpp
 
 LOCAL_SHARED_LIBRARIES := \
     libui
 
-LOCAL_MODULE := \
-	libshim_camera \
-	libshim_atomic
-
-
+LOCAL_MODULE := libshim_camera
 LOCAL_MODULE_TAGS := optional
 LOCAL_MULTILIB := 32
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := atomic.cpp
+LOCAL_MODULE := libshim_atomic
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := CameraParameters.cpp
+
+LOCAL_MODULE := libshim_camera_parameters
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_32_BIT_ONLY := true
 
 include $(BUILD_SHARED_LIBRARY)
