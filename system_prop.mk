730aeb0a7cbe7aab6ef8a3e62b57c1563ffe6b2d
coavasic <coavasic96@gmail.com>
2017-11-08 13:52:00 +0100

    b1w: disable SDCardFS

--- system_prop.mk
+++ system_prop.mk
@@ -74,6 +74,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
     rild.libpath=/vendor/lib/libril-qc-qmi-1.so \
     ro.ril.telephony.mqanelements=5
 
+# SDCardFS
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.sys.sdcardfs=false
+
+
+
 
 # Sensors
 PRODUCT_PROPERTY_OVERRIDES += \
