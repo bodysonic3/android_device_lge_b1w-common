ace6bf7a24b8e994b67f56562406201826f07f4b
coavasic <coavasic96@gmail.com>
2017-11-08 13:58:26 +0100

    audio: add service and soundtrigger

--- b1w.mk
+++ b1w.mk
@@ -66,7 +66,9 @@ $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
+    android.hardware.soundtrigger@2.0-impl \
     audio.a2dp.default \
     audio.primary.msm8974 \
     audio.r_submix.default \
