8bbe3fe927609bef1377a6cce17d4e5f6b7499ee
coavasic <coavasic96@gmail.com>
2017-11-08 13:33:07 +0100

    b1w:  Build libbt-vendor

--- b1w.mk
+++ b1w.mk
@@ -84,7 +84,7 @@ PRODUCT_COPY_FILES += \
 
 # Bluetooth
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl 
+    android.hardware.bluetooth@1.0-impl \
     libbt-vendor \
     hwaddrs
 
