LineageOS/android_device_lge_g2-common

g2-common: rebrand cmhw -> lineagehw
Change-Id: I34bb802c213379bb92752812391e69ed379a56bf

BoardConfigCommon.mk
@@ -62,8 +62,8 @@ BOARD_CHARGING_CMDLINE_NAME := "androidboot.mode"
 BOARD_CHARGING_CMDLINE_VALUE := "chargerlogo"
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
-# CMHW
-BOARD_HARDWARE_CLASS := device/lge/g2-common/cmhw/
+# LINEAGEHW
+BOARD_HARDWARE_CLASS := device/lge/g2-common/lineagehw/
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/touch_gesture"
 
 # Display

bodysonic3/android_device_lge_b1w-common

BoardConfigCommon.mk
@@ -68,9 +68,11 @@ BOARD_CHARGING_CMDLINE_NAME := "androidboot.mode"
 BOARD_CHARGING_CMDLINE_VALUE := "chargerlogo"
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
-# CMHW
-BOARD_HARDWARE_CLASS := device/lge/b1w-common/cmhw/
+
+# LINEAGEHW
+BOARD_HARDWARE_CLASS := device/lge/b1w-common/lineagehw/
 TARGET_TAP_TO_WAKE_NODE := "/sys/devices/virtual/input/lge_touch/dt2w_enable"
+
 
 # Display
 HAVE_ADRENO_SOURCE := false